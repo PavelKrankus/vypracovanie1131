fe005e1130f74c935385130818fc570e5d41d903
PavelKrankus <102058463+PavelKrankus@users.noreply.github.com>
2022-05-12 19:24:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-var x = 4;
-var y = 2;
-function pocitadlo(x, y) {
+function pocitadlo(x = 4, y = 2) {
   console.log("Hodnota x = " + x + " " + "Hodnota y = " + y);
   console.log("Vykonávam matematické operácie");
   console.log(x + " + " + y + " = " + (x + y));
@@ -8,4 +6,4 @@ function pocitadlo(x, y) {
   console.log(x + " * " + y + " = " + x * y);
   console.log(x + " / " + y + " = " + x / y);
 }
-console.log(pocitadlo(x, y));
+console.log(pocitadlo((x = 4), (y = 2)));
