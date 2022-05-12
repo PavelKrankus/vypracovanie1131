bc249c37e28ed3188b9ee0b48a374535bb7391c5
PavelKrankus <102058463+PavelKrankus@users.noreply.github.com>
2022-05-12 19:16:47 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var x = 4;
+var y = 2;
+function pocitadlo(x, y) {
+  console.log("Hodnota x = " + x + " " + "Hodnota y = " + y);
+  console.log("Vykonávam matematické operácie");
+  console.log(x + " + " + y + " = " + (x + y));
+  console.log(x + " - " + y + " = " + (x - y));
+  console.log(x + " * " + y + " = " + x * y);
+  console.log(x + " / " + y + " = " + x / y);
+}
+console.log(pocitadlo(x, y));
